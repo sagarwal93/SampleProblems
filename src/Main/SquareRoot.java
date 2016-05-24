e791ebf662edc5cede44f640d9e5f89b8b11daca
Samarth <samarth.agarwal1993@gmail.com>
2016-05-24 01:52:05 -0400

    Initial commit

---
+++ src/Main/SquareRoot.java
@@ -0,0 +1,47 @@
+package Main;
+
+/**
+ * Created by Samarth on 5/24/16.
+ */
+public class SquareRoot {
+
+    public static double getSquareRoot(double num, double tolerance) {
+
+        if (num < 0) {
+            return -1.0;
+        }
+
+        return getSquareRootHelper(num, 0, num, tolerance);
+    }
+
+    public static double getSquareRootHelper(double num, double min, double max, double tolerance) {
+
+        double rt = (min + max)/2;
+        double sqr = rt*rt;
+
+        if (checkSquare(num, sqr, tolerance)) {
+            return rt;
+        }
+        else if (sqr > num) {
+            return getSquareRootHelper(num, min, rt, tolerance);
+        }
+        else {
+            return getSquareRootHelper(num, rt, max, tolerance);
+        }
+    }
+
+    private static boolean checkSquare(double num, double sqr, double tolerance) {
+
+        double diff = num - sqr;
+
+        if (diff < 0) {
+            diff = -diff;
+        }
+
+        if (diff <= tolerance) {
+            return true;
+        }
+
+        return false;
+    }
+}
